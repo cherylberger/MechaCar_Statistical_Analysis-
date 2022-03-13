dccb1383184cafc5b6e4d713508015766390e467
cherylberger <94234511+cherylberger@users.noreply.github.com>
2022-03-13 00:43:11 -0500

    Add files via upload
    Upload MetaCarChallenge.RScript

---
+++ MechaCarChallenge.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(tidyverse)
+mechaCar_mpg <- read.csv(file="./Resources/MechaCar_mpg.csv", check.names =F, stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mpg))
+
